cf673fea61e68963f719f2890aee4330a1a3a855
diosyahrizal <diosyahrizal@gmail.com>
2022-08-31 22:46:52 +0700

    fix: rearrage folder project

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,7 +1,8 @@
+import clsx from "clsx";
 import React, { FC } from "react";
 
 const Content: FC = ({ children }) => {
-  return <div className="mx-auto max-w-6xl ">{children}</div>;
+  return <div className={clsx("mx-auto w-6xl")}>{children}</div>;
 };
 
 export default Content;
