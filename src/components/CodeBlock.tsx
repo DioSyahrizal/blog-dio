aca1b565bce03a3e166e969a1076e23a98d86c85
diosyahrizal <diosyahrizal@gmail.com>
2023-09-16 22:50:53 +0700

    fix: temporarely remove syntax highlighting

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -5,16 +5,7 @@ import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
 const CodeBlock = {
   code({ node, inline, className, children, ...props }: any) {
     const match = /language-(\w+)/.exec(className || "");
-    return !inline && match ? (
-      <SyntaxHighlighter
-        style={dracula}
-        language={match[1]}
-        PreTag="div"
-        {...props}
-      >
-        {String(children).replace(/\n$/, "")}
-      </SyntaxHighlighter>
-    ) : (
+    return (
       <code className={className} {...props}>
         {children}
       </code>
